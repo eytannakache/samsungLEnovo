71deaa04351eac28d0f2855b3ca2386f74747e1f
eitanakash <eitannakash@gmail.com>
2019-03-01 15:02:31 +0200

    if statment and importing random

--- demo.py
+++ demo.py
@@ -1,9 +1,18 @@
+import random
+
 print("my two laptops project")
 print("another line")
 
 
-
-
 def whos_num_bigger(user_num):
-    if 6 > user_num:
-        print    ("your num is bigger")
+    laptop_num = random.randint(0, 10)
+
+    if laptop_num < user_num:
+
+        print("your num is bigger \n I tought about {}".format(laptop_num))
+
+    else:
+        print("my num is bigger\n I tought about {}".format(laptop_num))
+
+
+whos_num_bigger(int(input(" give me number between 0-10:  \n")))
